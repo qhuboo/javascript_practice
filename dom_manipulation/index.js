345d05e22ca1eea8748bdb5f768fcce5f42b19cb
Jose Lopez Renteria <lucas@Joses-Mac-mini.attlocal.net>
2023-05-05 22:50:07 -0500

    more work

--- dom_manipulation/index.js
+++ dom_manipulation/index.js
@@ -3,3 +3,54 @@ const view1 = document.getElementById("view1");
 console.log(view1);
 
 const view2 = document.querySelector("#view2");
+
+view1.style.display = "flex";
+view2.style.display = "flex";
+
+const views = document.getElementsByClassName("view");
+console.log(views);
+
+const sameViews = document.querySelectorAll(".view");
+console.log(sameViews);
+
+const divs = view1.querySelectorAll("div");
+console.log(divs);
+
+const sameDivs = view1.getElementsByTagName("div");
+console.log(sameDivs);
+
+const evenDivs = view1.querySelectorAll("div:nth-of-type(2n)");
+console.log(evenDivs);
+
+for (let i = 0; i < evenDivs.length; i++) {
+  evenDivs[i].style.backgroundColor = "darkblue";
+}
+
+const navText = document.querySelector("nav h1");
+console.log(navText);
+const navTextContent = navText.textContent;
+const navInnerHTML = navText.innerHTML;
+console.log(`This is the result of the text content: ${navTextContent}`);
+console.log(`This is the result of the inner html: ${navInnerHTML}`);
+
+const navBar = document.querySelector("nav");
+console.log(navBar);
+navBar.innerHTML = "<h1>Hello!</h1> <p>This is some text</p>";
+navBar.style.justifyContent = "flex-start";
+navBar.innerHTML += "<h2>Adding another header</h2>";
+
+const newNavText = document.querySelector("nav h1");
+navText.innerHTML = "Hellooooooooo"; // This doesn't change anything since the navText has been overwritten by the navBar.innerHTML statement above.
+
+console.log(evenDivs[1].parentElement.children);
+console.log(evenDivs[0].parentElement.childNodes);
+
+console.log(evenDivs[0].parentElement.lastChild);
+console.log(evenDivs[0].parentElement.lastElementChild);
+
+const testDiv = document.querySelector(".test");
+console.log(testDiv);
+console.log(testDiv.lastChild);
+console.log(testDiv.lastElementChild);
+console.log(evenDivs[0].nextSibling);
+console.log(evenDivs[0].nextElementSibling);
