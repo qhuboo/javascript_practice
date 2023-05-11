dd60be257e608c038f4d8caabc9b1d56fcdb74b6
Jose Lopez Renteria <joselukas24@gmail.com>
2023-05-11 17:03:13 -0500

    done with starter files

--- event_target/main.js
+++
@@ -1,6 +0,0 @@
-const div = document.getElementById("first");
-if (div.classList.contains("two")) {
-  console.log("The div selected does indeed contain a class of name two");
-} else {
-  console.log("The dive selected does not contain a class of name two");
-}
