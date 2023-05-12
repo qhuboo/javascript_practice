2891ee6ddb2e75427c8c1390e82cd929aab39557
Jose Lopez Renteria <joselukas24@gmail.com>
2023-05-11 19:54:28 -0500

    done

--- event_listeners/main.js
+++ event_listeners/main.js
@@ -27,33 +27,61 @@ document.addEventListener("readystatechange", (event) => {
   }
 });
 
+// initApp that for the even listener examples with divs and other elements
+// const initApp = () => {
+//   const view = document.querySelector("#view2");
+//   const div = view.querySelector("div");
+//   const h2 = div.querySelector("h2");
+//   view.addEventListener(
+//     "click",
+//     (event) => {
+//       // view.style.backgroundColor = "purple";
+//       view.classList.toggle("purple");
+//       view.classList.toggle("darkblue");
+//     },
+//     true
+//   );
+//   div.addEventListener(
+//     "click",
+//     (event) => {
+//       // div.style.backgroundColor = "blue";
+//       div.classList.toggle("blue");
+//       div.classList.toggle("black");
+//     },
+//     false
+//   );
+//   h2.addEventListener(
+//     "click",
+//     (event) => {
+//       const myText = event.target.textContent;
+//       myText === "My 2nd View"
+//         ? (event.target.textContent = "Clicked!")
+//         : (event.target.textContent = "My 2nd View");
+//     },
+//     false
+//   );
+
+//   // Now for the form submit event
+//   const nav = document.querySelector("nav");
+//   nav.addEventListener("mouseover", (event) => {
+//     nav.classList.add("height100");
+//   });
+//   nav.addEventListener("mouseout", (event) => {
+//     nav.classList.remove("height100");
+//   });
+// };
+
+// Now we move onto the input form
+document.querySelector("#view1").style.display = "none";
+document.querySelector("#view2").style.display = "none";
+document.querySelector("#view3").style.display = "flex";
+
+// The initApp for the form event listener example
 const initApp = () => {
-  const view = document.querySelector("#view2");
-  const div = view.querySelector("div");
-  const h2 = div.querySelector("h2");
-  view.addEventListener(
-    "click",
-    (event) => {
-      // view.style.backgroundColor = "purple";
-      event.target.style.backgroundColor = "purple";
-      console.log(event.target);
-    },
-    true
-  );
-  div.addEventListener(
-    "click",
-    (event) => {
-      // div.style.backgroundColor = "blue";
-      event.target.style.backgroundColor = "blue";
-    },
-    false
-  );
-  h2.addEventListener(
-    "click",
-    (event) => {
-      // h2.textContent = "Clicked!";
-      event.target.textContent = "Clicked!";
-    },
-    false
-  );
+  const view3 = document.querySelector("#view3");
+  const myForm = view3.querySelector("#myForm");
+  myForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log("submit event");
+  });
 };
