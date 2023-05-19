7e4bc100f06abb1de8cd133a22712c7ae20ee8cc
Jose Lopez Renteria <joselukas24@gmail.com>
2023-05-19 14:52:05 -0500

    done with classes

--- classes/main.js
+++ classes/main.js
@@ -1,12 +1,59 @@
 // Javascript Classes
+// class Pizza {
+//   constructor(pizzaType, pizzaSize) {
+//     this.type = pizzaType;
+//     this.size = pizzaSize;
+//     this.crust = "original";
+//     this.toppings = [];
+//   }
+//   get pizzaCrust() {
+//     return this.crust;
+//   }
+//   set pizzaCrust(pizzaCrust) {
+//     this.crust = pizzaCrust;
+//   }
+//   getToppings() {
+//     return this.toppings;
+//   }
+//   setToppings(topping) {
+//     this.toppings.push(topping);
+//   }
+//   bake() {
+//     console.log(
+//       `Baking a ${this.size} ${this.type} ${this.crust} crust pizza.`
+//     );
+//   }
+// }
+// const myPizza = new Pizza("pepperoni", "small");
+// myPizza.pizzaCrust = "thin";
+// myPizza.setToppings("sausage");
+// myPizza.setToppings("olives");
+// console.log(myPizza.getToppings());
+
+// We will now reduce our Pizza class to use a parent(super) class
+// Super Class
 class Pizza {
-  constructor() {
-    this.size = "medium";
+  constructor(pizzaSize) {
+    this.size = pizzaSize;
     this.crust = "original";
   }
-  bake() {
-    console.log(`Baking a ${this.size} ${this.crust} crust pizza.`);
+  get pizzaCrust() {
+    return this.crust;
+  }
+  set pizzaCrust(pizzaCrust) {
+    this.crust = pizzaCrust;
   }
 }
-const myPizza = new Pizza();
-myPizza.bake();
+// Instance(child) of the super class
+// class specialtyPizza extends Pizza {
+//   constructor(pizzaSize) {
+//     super(pizzaSize);
+//     this.type = "The Works";
+//   }
+//   slice() {
+//     console.log(`Our ${this.type} ${this.size} pizza has 8 slices.`);
+//   }
+// }
+
+// const mySpecialty = new specialtyPizza("medium");
+// mySpecialty.slice();
