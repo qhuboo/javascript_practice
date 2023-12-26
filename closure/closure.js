6a34541daf225522265c4715be18f7b3d83f0a64
Jose Lopez Renteria <joselukas24@gmail.com>
2023-12-26 15:05:23 -0600

    working on closure challenges

--- closure/closure.js
+++ closure/closure.js
@@ -9,15 +9,76 @@
 
 // Challenges
 // 1. Create a function createFunction that creates and returns a function. When that created function is called, it should print "hello."
-function createFunction() {
-  function hello() {
-    console.log("hello");
+// function createFunction() {
+//   function hello() {
+//     console.log("hello");
+//   }
+//   return hello;
+// }
+
+// const newFunction = createFunction();
+// newFunction();
+
+// 2. Create a function createFunctionPrinter that accepts one input and returns a function. When that created function is called, it should print out the input
+// that was used when the function was created.
+// function createFunctionPrinter(input) {
+//   function printInput() {
+//     console.log(input);
+//   }
+//   return printInput;
+// }
+
+// const printer = createFunctionPrinter("Print this please");
+// printer();
+
+// 3. Examine the code for the outer function. Notice that we are returning a function and that function is using variables outside of it's scope. Uncomment those
+// lines of code. Try to deduce the output before executing. Now we are going to create a function addByX that returns a function that will add an input by x.
+// function outer() {
+//   let counter = 0;
+//   function incrementCounter() {
+//     counter++;
+//     console.log("counter", counter);
+//   }
+//   return incrementCounter;
+// }
+// const willCounter = outer();
+// const jasCounter = outer();
+
+// Uncomment each of these lines one by one.
+// Before you do, guess what will be logged from each function call.
+// /*** Uncomment these to check you work! ***/
+// willCounter(); // 1
+// willCounter(); // 2
+// willCounter(); // 3
+
+// jasCounter(); // 1
+// willCounter(); // 4
+
+// 4. Write a function once that accepts a callback as input and returns a function. When the returned function is called the first the first time,
+// it should call the callback and return that output. If it is called any additional times, instead of calling the callback again it will simply return the
+// output value from the first time it was called.
+function createOnceFunction(callback) {
+  let counter = 0;
+  let first = 0;
+  function once(input) {
+    if (counter == 0) {
+      first = callback(input);
+      console.log(`Run ${counter + 1}`, first);
+      counter++;
+      return first;
+    } else {
+      console.log(`Run ${counter + 1}`, first);
+      counter++;
+      return first;
+    }
   }
-  return hello;
+  return once;
 }
 
-const newFunction = createFunction();
-newFunction();
+const onceFunction = createOnceFunction((input) => input * 2);
+onceFunction(25);
+onceFunction(3);
+onceFunction(23);
 
-2. Create a function createFunctionPrinter that accepts one input and returns a function. When that created function is called, it should print out the input
-that was used when the function was created.
+// 5. Write a function *after* that takes the number of times the callback needs to be called before being executed as the first parameter
+// and the callback as the second parameter.
