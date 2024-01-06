1ff4123a87d44ce515ffbadd00332bbdc5fe5cda
Jose Lopez Renteria <joselukas24@gmail.com>
2024-01-05 21:17:19 -0600

    Done

--- async_await/asynchronous_js_frontendmasters.js
+++ async_await/asynchronous_js_frontendmasters.js
@@ -59,8 +59,7 @@
 
 // brokenRecord();
 
-// 5. Create a function *limitedRepeater* that console logs *hi for now* every second, but only for 5 seconds. Research how to use *clearInterval* if you are not sure
-// how to do this.
+// 5. Create a function *limitedRepeater* that console logs *hi for now* every second, but only for 5 seconds. Research how to use *clearInterval* if you are not sure// how to do this.
 // function limitedRepeater() {
 //   return setInterval(() => console.log("hi for now"), 1000);
 // }
@@ -104,10 +103,57 @@
 
 // 8. Write a function, *promised*, that takes in a value. This function will return a promise that will resolve after 2 seconds.
 // Hint: Take a look at the Promise object docs on MDN
-function promised(input) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => resolve(input), 2000);
-  });
+// function promised(input) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => resolve(input), 2000);
+//   });
+// }
+
+// promised(2).then((result) => console.log(result));
+
+// 9. Write a *SecondClock* class, with two methods: *start* and *reset*.
+// Start: Upon invocation, invokes a callback(this.cb, defined in the contructor) on an argument every second, with the argument to the callback being the current
+// secondds "value".
+// In other words, the callback gets invoked every second on the "seconds hand" of the clock. Always start with 1 and don't utilize the seconds value of
+// the current computer clock time.
+// 1. The first "tick" with value 1 occurs 1 second after the initial "secondClock" invocation.
+// 2. The second "tick" with value 2 occurs 2 seconds after the initial "secondClock" invocation.
+//              .
+//              .
+//              .
+// 60. The sixtieth "tick" with value 60 occurs 60 seconds after the initial "secondClock" invocation
+// etc.
+// Reset: Upon invocation, completely stops the "clock".
+// Also resets the time back to the beginning.
+class SecondClock {
+  constructor(callback) {
+    this.cb = callback;
+    this.timer = null;
+    this.counter = 0;
+  }
+  start() {
+    if (this.timer) {
+      // If the timer is already running, don't start another one
+      return;
+    }
+    this.timer = setInterval(() => {
+      this.counter = (this.counter % 60) + 1; // Increment the counter and reset to 1 after 60
+      this.cb(this.counter); // Call the callback with the current counter value
+    }, 1000); // Set interval for 1 second
+  }
+
+  reset() {
+    clearInterval(this.timer); // Stop the interval
+    this.timer = null;
+    this.counter = 0; // Reset the counter
+  }
 }
 
-promised(2).then((result) => console.log(result));
+const timer = new SecondClock((input) => console.log(input));
+timer.start();
+
+// 10. Write a function called *debounce* that accepts a function and returns a new function that only allows invocation of the given function
+// after *interval* milliseconds have passed since the last time the returned function ran.
+// Additional calls to the returned function within the *interval* time should not be invoked or queued, but the timer should get reset.
+
+// Will do this another time
